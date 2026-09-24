c6dc2bee195bfc11d7b6fb792fc5cc0dc9e1126e
TinyThinker <77919316+TinyThinker@users.noreply.github.com>
2026-09-24 00:03:04 -0700

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,29 @@
+class TimeMap:
+
+    def __init__(self):
+        self.store = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.store:
+            self.store[key] = []
+        self.store[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        if key not in self.store:
+            return res
+        
+        values = self.store[key]
+        l, r = 0, len(values)
+        while l < r:
+            mid = l + (r - l) // 2
+            if values[mid][1] <= timestamp:
+                l = mid + 1
+            else:
+                r = mid
+
+        if values[l-1][1] <= timestamp:
+            return values[l-1][0]
+
+        return ""
+        
